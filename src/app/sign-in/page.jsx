75d4399668ea8be8ac26a8f53fda729b25791146
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-09 15:58:14 +0000

    Tue, Jul 9, 2024, 3:58 PM +00:00

--- src/app/sign-in/page.jsx
+++ src/app/sign-in/page.jsx
@@ -27,7 +27,7 @@ const SignIn = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900">
+    <div className="min-h-screen flex items-center justify-center bg-white">
       <div className="bg-gray-800 p-10 rounded-lg shadow-xl w-96">
         <h1 className="text-white text-2xl mb-5">Sign In</h1>
         {error && <p className="text-red-500 mb-4">Error Logging in</p>}
