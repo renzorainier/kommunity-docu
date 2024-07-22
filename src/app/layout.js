b0eeb0e7d322cc4daaab20fa56104ea54542d5e9
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-22 12:12:41 +0000

    Mon, Jul 22, 2024, 12:12 PM +00:00

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
-
+// layout.js
 import "./globals.css";
-import { Urbanist } from 'next/font/google'
+import { Urbanist } from 'next/font/google';
+import { UserDataProvider } from '@/UserDataContext';
 
-const inter = Urbanist({ subsets: ['latin'] })
+const inter = Urbanist({ subsets: ['latin'] });
 
 export const metadata = {
   title: "MVBA",
@@ -12,7 +13,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} >{children}</body>
+      <body className={inter.className}>
+        <UserDataProvider>
+          {children}
+        </UserDataProvider>
+      </body>
     </html>
   );
 }
+
+
+
+// import "./globals.css";
+// import { Urbanist } from 'next/font/google'
+
+// const inter = Urbanist({ subsets: ['latin'] })
+
+// export const metadata = {
+//   title: "MVBA",
+//   description: "Metro View Baptist Academy",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body className={inter.className} >{children}</body>
+//     </html>
+//   );
+// }
