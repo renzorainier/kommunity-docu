30c28c983318c7e5902533e9f775fe071882d861
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-22 12:13:34 +0000

    Mon, Jul 22, 2024, 12:13 PM +00:00

---
+++ src/app/UserDataProvider .jsx
@@ -0,0 +1,16 @@
+// UserDataContext.js
+import { createContext, useContext, useState } from 'react';
+
+const UserDataContext = createContext();
+
+export const UserDataProvider = ({ children }) => {
+  const [userData, setUserData] = useState(null);
+
+  return (
+    <UserDataContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </UserDataContext.Provider>
+  );
+};
+
+export const useUserData = () => useContext(UserDataContext);
