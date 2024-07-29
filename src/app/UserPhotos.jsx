82da2efe2c3aa632a91f6818abc40ea6551f56d7
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-29 13:46:20 +0000

    Mon, Jul 29, 2024, 1:46 PM +00:00

--- src/app/UserPhotos.jsx
+++ src/app/UserPhotos.jsx
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from 'react';
 import { ref, getDownloadURL, listAll } from 'firebase/storage';
 import { storage } from './firebase'; // Adjust the path as needed
+import { CgProfile } from "react-icons/cg";
 
 const UserPhotos = ({ userId }) => {
   const [imageUrls, setImageUrls] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     if (userId) {
       const imagesListRef = ref(storage, `images/${userId}/`);
-      listAll(imagesListRef).then((response) => {
-        const fetchImageUrls = response.items.map((item) =>
-          getDownloadURL(item).then((url) => url)
-        );
-        Promise.all(fetchImageUrls).then((urls) => setImageUrls(urls));
-      });
+      listAll(imagesListRef)
+        .then((response) => {
+          if (response.items.length === 0) {
+            setError(true);
+            return;
+          }
+          const fetchImageUrls = response.items.map((item) =>
+            getDownloadURL(item).then((url) => url).catch(() => setError(true))
+          );
+          Promise.all(fetchImageUrls)
+            .then((urls) => setImageUrls(urls))
+            .catch(() => setError(true));
+        })
+        .catch(() => setError(true));
     }
   }, [userId]);
 
@@ -21,9 +31,13 @@ const UserPhotos = ({ userId }) => {
     <div>
       <h2>Your Photos</h2>
       <div className="photos-grid">
-        {imageUrls.map((url) => (
-          <img src={url} alt="User Uploaded" key={url} className="photo" />
-        ))}
+        {error || imageUrls.length === 0 ? (
+          <CgProfile size={100} />
+        ) : (
+          imageUrls.map((url) => (
+            <img src={url} alt="User Uploaded" key={url} className="photo" />
+          ))
+        )}
       </div>
     </div>
   );
