b1550b113518a6b6a72962b59ab9526a3fc83f51
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-30 02:51:47 +0000

    Tue, Jul 30, 2024, 2:51 AM +00:00

--- src/app/Profile.jsx
+++ src/app/Profile.jsx
@@ -3,7 +3,7 @@ import { ref, getDownloadURL, listAll } from 'firebase/storage';
 import { storage } from './firebase'; // Adjust the path as needed
 import { CgProfile } from "react-icons/cg";
 
-const UserPhotos = ({ userId, userData }) => {
+const Profile = ({ userId, userData }) => {
   const [imageUrl, setImageUrl] = useState('');
   const [error, setError] = useState(false);
 
@@ -48,4 +48,4 @@ const UserPhotos = ({ userId, userData }) => {
   );
 };
 
-export default UserPhotos;
+export default Profile;
