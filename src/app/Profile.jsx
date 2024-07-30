33ec971760ec424e6266f64cb12d516bd47afabb
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-30 03:26:22 +0000

    Tue, Jul 30, 2024, 3:26 AM +00:00

--- src/app/Profile.jsx
+++ src/app/Profile.jsx
@@ -38,29 +38,29 @@ const Profile = ({ userId, userData }) => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-[#031525]">
       <div className="max-w-5xl mx-auto p-8 bg-white shadow-lg rounded-lg">
-        <div className="bg-gradient-to-r from-[#035172] to-[#0587be] p-6 rounded-t-lg shadow-lg text-center">
-          <h2 className="text-3xl font-bold text-white">Student Profile</h2>
-        </div>
-        <div className="flex justify-center items-center mb-12 mt-4">
-          {error || !imageUrl ? (
-            <CgProfile size={120} className="text-gray-400" />
-          ) : (
-            <img src={imageUrl} alt="User Uploaded" className="w-48 h-48 rounded-full object-cover shadow-md" />
-          )}
-        </div>
-        <div className="bg-gray-50 p-8 rounded-lg shadow-inner">
-          <h2 className="text-2xl font-semibold text-gray-800 mb-6">Student Information</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-            <p className="text-gray-700"><strong>Name:</strong> {userData.name}</p>
-            <p className="text-gray-700"><strong>Grade Level:</strong> {userData.level}</p>
-            <p className="text-gray-700"><strong>Schedule:</strong> {userData.schedule}</p>
-            <p className="text-gray-700"><strong>Adviser:</strong> {userData.adviser}</p>
-            <p className="text-gray-700"><strong>Gender:</strong> {userData.isMale ? 'Male' : 'Female'}</p>
+        <div className="bg-gradient-to-r from-[#035172] to-[#0587be] p-6 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+          <div className="flex justify-center items-center mb-12">
+            {error || !imageUrl ? (
+              <CgProfile size={120} className="text-gray-400" />
+            ) : (
+              <img src={imageUrl} alt="User Uploaded" className="w-48 h-48 rounded-full object-cover shadow-md" />
+            )}
+          </div>
+          <div className="bg-gray-50 p-8 rounded-lg shadow-inner">
+            <h2 className="text-2xl font-semibold text-gray-800 mb-6 text-center">Student Information</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+              <p className="text-gray-700"><strong>Name:</strong> {userData.name}</p>
+              <p className="text-gray-700"><strong>Grade Level:</strong> {userData.level}</p>
+              <p className="text-gray-700"><strong>Schedule:</strong> {userData.schedule}</p>
+              <p className="text-gray-700"><strong>Adviser:</strong> {userData.adviser}</p>
+              <p className="text-gray-700"><strong>Gender:</strong> {userData.isMale ? 'Male' : 'Female'}</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
   );
+
 
 };
 
