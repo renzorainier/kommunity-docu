1d53c49381574660ca72b61df6fabf4bd13e4da9
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-07-30 03:13:17 +0000

    Tue, Jul 30, 2024, 3:13 AM +00:00

--- src/app/Profile.jsx
+++ src/app/Profile.jsx
@@ -8,35 +8,44 @@ const Profile = ({ userId, userData }) => {
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    if (userId) {
-      const imagesListRef = ref(storage, `images/${userId}/`);
-      listAll(imagesListRef)
-        .then((response) => {
-          if (response.items.length === 0) {
+    const fetchImage = async () => {
+      const storedImageUrl = localStorage.getItem(`user-${userId}-image`);
+      if (storedImageUrl) {
+        setImageUrl(storedImageUrl);
+      } else {
+        if (userId) {
+          const imagesListRef = ref(storage, `images/${userId}/`);
+          try {
+            const response = await listAll(imagesListRef);
+            if (response.items.length === 0) {
+              setError(true);
+              return;
+            }
+            const firstItem = response.items[0];
+            const url = await getDownloadURL(firstItem);
+            setImageUrl(url);
+            localStorage.setItem(`user-${userId}-image`, url);
+          } catch (err) {
             setError(true);
-            return;
           }
-          // Get the first image URL
-          const firstItem = response.items[0];
-          getDownloadURL(firstItem)
-            .then((url) => setImageUrl(url))
-            .catch(() => setError(true));
-        })
-        .catch(() => setError(true));
-    }
+        }
+      }
+    };
+
+    fetchImage();
   }, [userId]);
+
 
   return (
     <div className="flex items-center justify-center min-h-screen ">
-      <div className="max-w-5xl mx-auto p-8 bg-white shadow-lg rounded-lg">
-        <h2 className="text-3xl font-bold text-gray-900 mb-8 text-center"></h2>
-        <div className="flex justify-center items-center mb-12">
-          {error || !imageUrl ? (
-            <CgProfile size={120} className="text-gray-400" />
-          ) : (
-            <img src={imageUrl} alt="User Uploaded" className="w-48 h-48 rounded-full object-cover shadow-md" />
-          )}
-        </div>
+    <div className="max-w-5xl mx-auto p-8 bg-white shadow-lg rounded-lg">
+      <div className="flex justify-center items-center mb-12">
+        {error || !imageUrl ? (
+          <CgProfile size={120} className="text-gray-400" />
+        ) : (
+          <img src={imageUrl} alt="User Uploaded" className="w-48 h-48 rounded-full object-cover shadow-md" />
+        )}
+      </div>
         <div className="bg-gray-50 p-8 rounded-lg shadow-inner">
           <h2 className="text-2xl font-semibold text-gray-800 mb-6">Student Information</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
