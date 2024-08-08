9c226ba5bb89d44eef266254a02f66bbeb01a4e0
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-08-08 08:42:37 +0000

    Thu, Aug 8, 2024, 8:42 AM +00:00

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,6 @@
-import withPWA from 'next-pwa';
+
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Other Next.js config options can go here
-};
+const nextConfig = {};
 
-export default withPWA({
-  dest: 'public'
-})(nextConfig);
-
-// const withPWA = require('next-pwa')({
-//     dest: 'public'
-//   });
-
-//   /** @type {import('next').NextConfig} */
-//   const nextConfig = {
-//     // Other Next.js config options can go here
-//   };
-
-//   module.exports = withPWA(nextConfig);
-
-// const withPWA = require('next-pwa')({
-//     dest: 'public'
-//   })
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
+export default nextConfig;
