6681d36b145107c058cef63f744c3b0e78327af9
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-08-09 02:31:26 +0000

    Fri, Aug 9, 2024, 2:31 AM +00:00

--- next.config.mjs
+++ next.config.mjs
@@ -13,14 +13,9 @@
 // // Export the combined configuration using withPWA
 // export default withPWA(pwaConfig)(nextConfig);
 
-import withPWA from 'next-pwa';
+
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Example of a valid Next.js config
-  // Add other Next.js config options here if needed
-};
+const nextConfig = {};
 
-export default withPWA({
-  dest: 'public',
-})(nextConfig);
+export default nextConfig;
