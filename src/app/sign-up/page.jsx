175a1dcbc5a1e084c255ac0b3922d092b0b1be11
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-11-28 06:43:17 +0000

    Thu, Nov 28, 2024, 2:43 PM +08:00

--- src/app/sign-up/page.jsx
+++ src/app/sign-up/page.jsx
@@ -387,6 +387,345 @@ const Register = () => {
 };
 
 export default Register;
+// //
+// "use client";
+
+
+// import Image from "next/image";
+// import { useState } from "react";
+// import { useRouter } from "next/navigation";
+// import { auth, db, storage } from "@/app/firebase/config";
+// import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+// import { doc, setDoc, getDoc } from "firebase/firestore";
+// import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+// import teen from "../img.png";
+
+
+// const skillOptions = [
+//   "Web Development",
+//   "Graphic Design",
+//   "Data Analysis",
+//   "Content Writing",
+//   "Marketing",
+//   "Project Management",
+//   "Software Testing",
+// ];
+
+
+// const Register = () => {
+//   const [formData, setFormData] = useState({
+//     firstName: "",
+//     surname: "",
+//     contactNumber: "",
+//     facebookLink: "",
+//     email: "",
+//     jobSkillset: [],
+//   });
+//   const [searchText, setSearchText] = useState("");
+//   const [filteredSkills, setFilteredSkills] = useState(skillOptions);
+//   const [uploadedImage, setUploadedImage] = useState(null);
+//   const [imageError, setImageError] = useState(false);
+//   const [showError, setShowError] = useState(false);
+//   const [loading, setLoading] = useState(false);
+//   const router = useRouter();
+
+
+//   const handleGoogleSignIn = async () => {
+//     setLoading(true);
+//     try {
+//       const provider = new GoogleAuthProvider();
+//       const result = await signInWithPopup(auth, provider);
+//       const user = result.user;
+
+
+//       const userRef = doc(db, "users", user.uid);
+//       const userDoc = await getDoc(userRef);
+
+
+//       if (!userDoc.exists()) {
+//         setFormData((prev) => ({
+//           ...prev,
+//           email: user.email,
+//         }));
+//       } else {
+//         alert("You are already registered! Welcome back.");
+//         router.push("/");
+//       }
+//     } catch (error) {
+//       console.error(error);
+//       setShowError(true);
+//       setTimeout(() => setShowError(false), 3000);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+
+//   const handleInputChange = (e) => {
+//     const { id, value } = e.target;
+//     setFormData((prev) => ({ ...prev, [id]: value }));
+//   };
+
+
+//   const handleSkillSearch = (e) => {
+//     const text = e.target.value;
+//     setSearchText(text);
+//     setFilteredSkills(
+//       skillOptions.filter(
+//         (skill) =>
+//           skill.toLowerCase().includes(text.toLowerCase()) &&
+//           !formData.jobSkillset.includes(skill)
+//       )
+//     );
+//   };
+
+
+//   const handleSkillAdd = (skill) => {
+//     setFormData((prev) => ({
+//       ...prev,
+//       jobSkillset: [...prev.jobSkillset, skill],
+//     }));
+//     setSearchText("");
+//     setFilteredSkills([]);
+//   };
+
+
+//   const handleSkillRemove = (skill) => {
+//     setFormData((prev) => ({
+//       ...prev,
+//       jobSkillset: prev.jobSkillset.filter((s) => s !== skill),
+//     }));
+//   };
+
+
+//   const handleImageUpload = (file) => {
+//     if (!file) {
+//       setImageError(true);
+//       return;
+//     }
+//     setImageError(false);
+//     setUploadedImage(file);
+//   };
+
+
+//   const handleFilePicker = (e) => {
+//     const file = e.target.files[0];
+//     handleImageUpload(file);
+//   };
+
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     setLoading(true);
+
+
+//     try {
+//       const user = auth.currentUser;
+//       if (!user) throw new Error("User is not authenticated");
+
+
+//       let imageUrl = "";
+//       if (uploadedImage) {
+//         const storageRef = ref(storage, `images/${user.uid}/${uploadedImage.name}`);
+//         await uploadBytes(storageRef, uploadedImage);
+//         imageUrl = await getDownloadURL(storageRef);
+//       }
+
+
+//       const fullName = `${formData.firstName} ${formData.surname}`; // Merge names
+
+
+//       const userRef = doc(db, "users", user.uid);
+//       await setDoc(userRef, {
+//         name: fullName, // Store merged name
+//         contactNumber: formData.contactNumber,
+//         facebookLink: formData.facebookLink,
+//         email: formData.email,
+//         jobSkillset: formData.jobSkillset,
+//         imageUrl,
+//         userID: user.uid,
+//       });
+
+
+//       alert("Registration complete! Welcome, " + fullName);
+//       router.push("/");
+//     } catch (error) {
+//       console.error("Error submitting form: ", error);
+//       setShowError(true);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+
+
+
+//   const isFormComplete =
+//     formData.firstName &&
+//     formData.surname &&
+//     formData.contactNumber &&
+//     formData.facebookLink &&
+//     uploadedImage;
+
+
+//   return (
+//     <div className="min-h-screen flex items-center justify-center bg-gray-300 p-4">
+//       <div className="bg-white rounded-lg shadow-lg flex flex-col w-full max-w-lg">
+//         <div className="w-full p-8 flex flex-col justify-center items-center bg-gradient-to-br from-gray-900 to-gray-800 text-white rounded-t-lg">
+//           <Image src={teen} width={260} height={260} alt="Teen Image" />
+//         </div>
+//         <div className="w-full p-8 flex flex-col justify-center items-center">
+//           <h1 className="text-4xl font-bold text-gray-800 mb-4">Create Your Account</h1>
+//           {showError && (
+//             <p className="text-red-500 mb-4 text-center">
+//               Error: Please try again later.
+//             </p>
+//           )}
+//           <button
+//             onClick={handleGoogleSignIn}
+//             className="w-full p-3 bg-white text-black font-bold rounded-lg shadow-md"
+//             disabled={loading}
+//           >
+//             {loading ? "Signing up with Google..." : "Sign up with Google"}
+//           </button>
+//           {formData.email && (
+//             <form onSubmit={handleSubmit} className="mt-6 space-y-6 w-full">
+//               <input
+//                 type="text"
+//                 id="firstName"
+//                 placeholder="First Name"
+//                 value={formData.firstName}
+//                 onChange={handleInputChange}
+//                 required
+//                 className="w-full border rounded-lg p-3"
+//               />
+//               <input
+//                 type="text"
+//                 id="surname"
+//                 placeholder="Surname"
+//                 value={formData.surname}
+//                 onChange={handleInputChange}
+//                 required
+//                 className="w-full border rounded-lg p-3"
+//               />
+//               <input
+//                 type="text"
+//                 id="contactNumber"
+//                 placeholder="Contact Number"
+//                 value={formData.contactNumber}
+//                 onChange={handleInputChange}
+//                 required
+//                 className="w-full border rounded-lg p-3"
+//               />
+//               <input
+//                 type="url"
+//                 id="facebookLink"
+//                 placeholder="Facebook Link"
+//                 value={formData.facebookLink}
+//                 onChange={handleInputChange}
+//                 required
+//                 className="w-full border rounded-lg p-3"
+//               />
+
+
+//               <div className="w-full">
+//                 <label className="block mb-2 font-bold">Search Skills:</label>
+//                 <input
+//                   type="text"
+//                   placeholder="Type a skill..."
+//                   value={searchText}
+//                   onChange={handleSkillSearch}
+//                   className="w-full border rounded-lg p-3"
+//                 />
+//                 {filteredSkills.length > 0 && (
+//                   <ul className="border mt-2 rounded-lg max-h-40 overflow-y-scroll">
+//                     {filteredSkills.map((skill) => (
+//                       <li
+//                         key={skill}
+//                         onClick={() => handleSkillAdd(skill)}
+//                         className="p-2 cursor-pointer hover:bg-gray-100"
+//                       >
+//                         {skill}
+//                       </li>
+//                     ))}
+//                   </ul>
+//                 )}
+//                 <div className="mt-4 flex flex-wrap gap-2">
+//                   {formData.jobSkillset.map((skill) => (
+//                     <div
+//                       key={skill}
+//                       className="bg-gray-200 rounded-full px-4 py-1 text-sm flex items-center gap-2"
+//                     >
+//                       {skill}
+//                       <button
+//                         type="button"
+//                         onClick={() => handleSkillRemove(skill)}
+//                         className="text-red-500 font-bold"
+//                       >
+//                         x
+//                       </button>
+//                     </div>
+//                   ))}
+//                 </div>
+//               </div>
+
+
+//               <div
+//                 className="w-full p-4 border border-dashed rounded-lg text-center cursor-pointer"
+//                 onClick={() => document.getElementById("file-upload").click()}
+//               >
+//                 <label htmlFor="file-upload" className="block w-full cursor-pointer">
+//                   {uploadedImage ? (
+//                     <p>{uploadedImage.name}</p>
+//                   ) : (
+//                     <p>Drag & drop an image here or click to select one</p>
+//                   )}
+//                 </label>
+//                 <input
+//                   id="file-upload"
+//                   type="file"
+//                   accept="image/*"
+//                   onChange={handleFilePicker}
+//                   className="hidden"
+//                 />
+//               </div>
+//               {imageError && (
+//                 <p className="text-red-500 text-sm mt-2">Please upload a valid image.</p>
+//               )}
+
+
+//               <button
+//                 type="submit"
+//                 className="w-full bg-blue-600 text-white font-bold py-2 rounded-lg"
+//                 disabled={!isFormComplete || loading}
+//               >
+//                 {loading ? "Submitting..." : "Complete Registration"}
+//               </button>
+//             </form>
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+// export default Register;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //workin ver 26
 
